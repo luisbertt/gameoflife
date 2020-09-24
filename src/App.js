074cd0801bc0f923f33043b12331ca5676e15a0c
luisbertt <luisbert2506@gmail.com>
2020-09-24 16:19:13 -0400

    Added clear and seed buttons functionality & centered the layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react"
 
-const Box = ({ id, row, col, selectBox, isActive }) => (
+const Box = ({ row, col, selectBox, isActive }) => (
   <div
     className={`w-5 h-5 border ${
       isActive ? "bg-blue-400" : "bg-white hover:bg-gray-400"
     }`}
-    id={id}
+    id={`${row}_${col}`}
     onClick={() => selectBox(row, col)}
   ></div>
 )
 
-const Grid = ({ grid, width, selectBox }) => (
-  <div style={{ width: `${width}rem` }} className="flex flex-wrap mx-auto mt-4">
+const Grid = ({ grid, selectBox }) => (
+  <div
+    style={{ width: `${grid.length * 1.25}rem` }}
+    className="flex flex-wrap mx-auto mt-4"
+  >
     {grid.map((row, i) =>
       row.map((col, j) => (
         <Box
-          id={`${i}_${j}`}
           key={`${i}_${j}`}
           row={i}
           col={j}
@@ -40,18 +42,36 @@ function App() {
       .map(() => Array(cols).fill(false))
   )
 
+  const isEmpty = grid => {
+    return !grid.some(row => row.includes(true))
+  }
+
   const selectBox = (row, col) => {
+    if (generation !== 0) setGeneration(0)
+    if (!isPaused) setIsPaused(true)
+
     let newGrid = [...grid]
     newGrid[row][col] = !newGrid[row][col]
     setGrid(newGrid)
   }
 
   const seed = () => {
+    pause()
     let newGrid = [...grid]
     newGrid.map((row, i) =>
       row.map((col, j) => (newGrid[i][j] = Math.floor(Math.random() * 4) === 1))
     )
     setGrid(newGrid)
+  }
+
+  const clear = () => {
+    if (!isPaused) pause()
+    setGeneration(0)
+    setGrid(
+      Array(rows)
+        .fill()
+        .map(() => Array(cols).fill(false))
+    )
   }
 
   const gameoflife = () => {
@@ -78,30 +98,34 @@ function App() {
   }
 
   const start = () => {
+    if (isEmpty(grid)) return
+
     setIsPaused(false)
-    clearInterval(intervalId)
+    setIntervalId(clearInterval(intervalId))
     setIntervalId(setInterval(gameoflife, speed))
   }
 
   const pause = () => {
     setIsPaused(true)
-    clearInterval(intervalId)
+    setIntervalId(clearInterval(intervalId))
   }
 
   useEffect(() => {
-    seed()
-  }, [])
+    if (isEmpty(grid)) pause()
+  }, [generation])
 
   return (
     <div className="text-center">
       <h1 className="text-3xl font-bold">Game of Life</h1>
       <div className="space-x-5 text-blue-500">
-        <button className="" onClick={isPaused ? start : pause}>
+        <button onClick={isPaused ? start : pause}>
           {isPaused ? "Start" : "Pause"}
         </button>
+        <button onClick={clear}>Clear</button>
+        <button onClick={seed}>Seed</button>
       </div>
-      <Grid grid={grid} width={cols * 1.25} selectBox={selectBox} />
-      <h2 className="font-bold">Generations: {generation}</h2>
+      <Grid grid={grid} selectBox={selectBox} />
+      <h2 className="font-bold mt-4">Generations: {generation}</h2>
     </div>
   )
 }
